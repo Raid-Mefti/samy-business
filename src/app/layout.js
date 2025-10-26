e875b61872eade79cbd25174c3b0eaf11d02aca7
Raid <raid.mefti@hotmail.com>
2025-10-26 23:18:23 +0000

    dark theme became default theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" data-theme="light">
+        <html lang="en" data-theme="dark">
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased bg-base-100 text-base-content`}
             >
